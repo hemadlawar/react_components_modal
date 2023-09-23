165346810c5f12ea32b17063f0fb0b9cb09af394
hemaddlawar <hema.200219627512@gmail.com>
2023-09-23 15:06:18 +0300

    install tailwind and create model page

--- src/components/modalpage.jsx
+++ src/components/modalpage.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Modalpage() {
-  return <div>M</div>;
+  const [showModal, setshowModal] = useState(true);
+  const eventHandling = () => {
+    setshowModal(!showModal);
+  };
+  return (
+    <div>
+      <div>
+        <p className=" f">
+          hiiii, this is a modal , i developed it to improve my programming
+          skills {";) "}
+        </p>
+      </div>
+      <div className="flex justify-center ">
+        <div className="bg-slate-100 w-72 h-16 text-center mt-3.5 ">
+          Open The Modal
+          <button
+            type="button"
+            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2  mt-3 ml-8 "
+            onClick={eventHandling}
+          >
+            Open
+          </button>
+        </div>
+        {showModal && <div> hhhhh</div>}
+      </div>
+    </div>
+  );
 }
